eb6bfde2b75afad2d6233dc8c250f3ebec0cb55a
RussC22 <114961971+RussC22@users.noreply.github.com>
2023-04-04 19:44:23 -0400

    database Update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,86 +1,50 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-// We are creating a new database named 'contact' which will be using version 1 of the database.
-  openDB('contact', 1, {
+  // We are creating a new database named 'contact' which will be using version 1 of the database.
+  openDB("jate", 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('contact')) {
-        console.log('contact database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
       // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-      db.createObjectStore('contact', { keyPath: 'id', autoIncrement: true });
-      console.log('contact database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // Export a function we will use to POST to the database.
-export const postDb = async (name, home, cell, email)  => {
-  console.log('Post to the database');
+export const postDb = async (content) => {
+  console.log("Put to the database");
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction("jate", "readwrite");
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore("jate");
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
-
-  // Get confirmation of the request.
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log("🚀 - data saved to the database", result.value);
 };
-;
 
-// Export a function we will use to GET to the database.
 export const getDb = async () => {
-  console.log('GET from the database');
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
-  // Get confirmation of the request.
+  console.log("GET from the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
   const result = await request;
-  console.log('result.value', result);
-  return result;
-};
-
-
-// Export a function we will use to DELETE to the database.
-export const deleteDb = async (id) => {
-  console.log('DELETE from the database', id);
-
-  // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('contact');
-
-  // Use the .delete() method to get all data in the database.
-  const request = store.delete(id);
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
+  result
+    ? console.log("🚀 - data retrieved from the database", result.value)
+    : console.log("🚀 - data not found in the database");
   return result?.value;
 };
-
-// Start the database.
+// Start the database
 initdb();
